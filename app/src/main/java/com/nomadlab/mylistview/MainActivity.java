23fd88af4200c2929cf5ef29aea459e7bce66fae
questzz@naver.com <questzz@naver.com>
2021-12-26 17:21:24 +0900

    init listView project

---
+++ app/src/main/java/com/nomadlab/mylistview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.nomadlab.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.nomadlab.mylistview.adapter.FoodListAdapter;
+import com.nomadlab.mylistview.model.Food;
+
+import java.util.ArrayList;
+
+/*
+*   1. 리스트로 만들고 싶은 아이템 리스트를 준비한다. (XML, 데이터)
+*   2. 코드에서 인플레이터를 준비한다.
+*   3. 인플레이터로 아이템 하나에 해당하는 뷰를 만들어 준다.
+*   4. 위에서 만든 뷰를 컨테이너 뷰에 붙여 준다.
+*
+*   ListView
+*   1. 리스트로 만들고 싶은 아이템의 리스트를 준비한다.
+*   2. Adapter 를 이용한다.
+*
+*   AddView 와 ListView 의 차이점
+*   1. 만드는 방식이 다르다.
+*   2. 그리는 방식이 다르다. (가장 큰 차이점)
+*       - AddView 는 리스의 갯수와 상관없이 한번에 다 그린다.
+*       - ListView 보여지는 부분 + 알파만 한번에 그리고 필요한 경우(스크롤시) 더 그린다.
+*
+* */
+public class MainActivity extends AppCompatActivity {
+
+    FoodListAdapter foodListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get sample data
+        ArrayList<Food> foods =  Food.getSampleData();
+        foodListAdapter = new FoodListAdapter(foods, this);
+        ListView listView = findViewById(R.id.foodListView);
+
+        listView.setAdapter(foodListAdapter);
+
+        // 이벤트 리스너 사용방법
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("TAG", " selected Id : " + i);
+                Intent intent = new Intent(view.getContext(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/nomadlab/mylistview/adapter/FoodListAdapter.java
@@ -0,0 +1,73 @@
+package com.nomadlab.mylistview.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.nomadlab.mylistview.R;
+import com.nomadlab.mylistview.model.Food;
+
+import java.util.ArrayList;
+
+public class FoodListAdapter extends BaseAdapter {
+
+    ArrayList<Food> data;
+    Context context; // LayoutInflater 사용하기 위해 전달 받음
+
+    public FoodListAdapter(ArrayList<Food> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+
+    /*전체 갯수를 알아야 adapter 가 조정할 수 있다.*/
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    // 데이터 하나를 알려 준다.
+    @Override
+    public Object getItem(int i) {
+        Log.d("TAG", "getItem i : " + i);
+        return data.get(i);
+    }
+
+    // 목록의 각 행에 대해 고유한 ID를 정의하는 메소드를 구현합니다 .
+    // 간단하게 하기 위해 항목의 위치를 보통 ID로 사용하면 됩니다.
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    // 레이아웃 파일과 데이터를 활용해서 각각 뷰를 그려준다.
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Log.d("TAG", "getView i : " + i);
+        // 그리고자 하는 아이템 하나(xml 이용)
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View itemView = inflater.inflate(R.layout.item_food, null);
+        ImageView imageView = itemView.findViewById(R.id.thumbnailImageView);
+        TextView titleTextView = itemView.findViewById(R.id.titleTextView);
+        TextView subTitleView = itemView.findViewById(R.id.subTitleTextView);
+        TextView detailTextView = itemView.findViewById(R.id.detailTextView);
+
+        Food food = data.get(i);
+
+        Glide.with(context)
+                .load(food.getThumbnail())
+                .centerCrop()
+                .into(imageView);
+
+        titleTextView.setText(food.getTitle());
+        subTitleView.setText(food.getSubTitle());
+        detailTextView.setText(food.getDetail());
+
+        return itemView;
+    }
+}
